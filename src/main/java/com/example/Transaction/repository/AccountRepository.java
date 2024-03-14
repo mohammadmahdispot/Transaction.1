a6c325e228e5693541c73bc7180229ea99382bef
M <mohammadmahdispot@gmail.com>
2024-03-14 13:43:27 +0330

    ACCOUNT REPOSITORY

---
+++ src/main/java/com/example/Transaction/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.Transaction.repository;
+
+import com.example.Transaction.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByAccountNumber(String accountNumber);
+}
